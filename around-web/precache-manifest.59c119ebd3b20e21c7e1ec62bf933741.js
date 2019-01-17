147d6e4d39b7a0328fdee2e57fa5c0272bc5adbe
HanhanAnderson <Hanhan@gwu.edu>
2019-01-17 00:49:35 -0500

    rebuild

---
+++ around-web/precache-manifest.59c119ebd3b20e21c7e1ec62bf933741.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc8cc4eae3d3822562e825fb5cec5536",
+    "url": "./static/media/blue-marker.bc8cc4ea.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fbb81a4ee7e616dae881",
+    "url": "./static/js/main.fbb81a4e.chunk.js"
+  },
+  {
+    "revision": "bddb4551b7c957fd956d",
+    "url": "./static/js/1.bddb4551.chunk.js"
+  },
+  {
+    "revision": "fbb81a4ee7e616dae881",
+    "url": "./static/css/main.b4f4f8e6.chunk.css"
+  },
+  {
+    "revision": "bddb4551b7c957fd956d",
+    "url": "./static/css/1.893a2ed3.chunk.css"
+  },
+  {
+    "revision": "c83760835ee4b7c3546c1fb3387b6f63",
+    "url": "./index.html"
+  }
+];
--- around-web/service-worker.js
+++ around-web/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/around-web/precache-manifest.918752e390586c535270b32db1218d85.js"
+  "./precache-manifest.59c119ebd3b20e21c7e1ec62bf933741.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/around-web/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
